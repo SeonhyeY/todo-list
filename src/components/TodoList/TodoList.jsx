c825d3bb4f8b120fc1b3804dbf5fb156d1333570
SeonhyeY <syoon330@gmail.com>
2023-08-22 03:09:42 -0600

    Function: filter items by its status; all, active, completed

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import AddTodo from '../AddTodo/AddTodo';
 import Todo from '../Todo/Todo';
 
-export default function TodoList() {
+export default function TodoList({ filter }) {
   const [todos, setTodos] = useState([
     { id: '123', text: 'Groceries', status: 'active' },
     { id: '124', text: 'Studying', status: 'active' },
@@ -15,10 +15,11 @@ export default function TodoList() {
     setTodos(todos.filter((item) => item.id !== deleted.id));
   };
 
+  const filtered = getFilteredItems(todos, filter);
   return (
     <section>
       <ul>
-        {todos.map((item) => (
+        {filtered.map((item) => (
           <Todo
             key={item.id}
             todo={item}
@@ -30,4 +31,11 @@ export default function TodoList() {
       <AddTodo onAdd={handleAdd} />
     </section>
   );
+}
+
+function getFilteredItems(todos, filter) {
+  if (filter === 'all') {
+    return todos;
+  }
+  return todos.filter((item) => item.status === filter);
 }
