cb43ad741eaec3eb2d6419afce993b02f44b9759
SeonhyeY <syoon330@gmail.com>
2023-08-22 02:45:11 -0600

    Funciton: item completion status with checkbox and delete item

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,20 +1,30 @@
 import React, { useState } from 'react';
 import AddTodo from '../AddTodo/AddTodo';
+import Todo from '../Todo/Todo';
 
 export default function TodoList() {
   const [todos, setTodos] = useState([
     { id: '123', text: 'Groceries', status: 'active' },
     { id: '124', text: 'Studying', status: 'active' },
   ]);
-  const handleAdd = (todo) => {
-    setTodos([...todos, todo]);
+  const handleAdd = (todo) => setTodos([...todos, todo]);
+  const handleUpdate = (updated) => {
+    setTodos(todos.map((item) => (item.id === updated.id ? updated : item)));
+  };
+  const handleDelete = (deleted) => {
+    setTodos(todos.filter((item) => item.id !== deleted.id));
   };
 
   return (
     <section>
       <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.text}</li>
+        {todos.map((item) => (
+          <Todo
+            key={item.id}
+            todo={item}
+            onUpdate={handleUpdate}
+            onDelete={handleDelete}
+          ></Todo>
         ))}
       </ul>
       <AddTodo onAdd={handleAdd} />
